b754f0c386a4ff18b2a1075e0deff100e913c921
RamDev <daudalaramadan2@gmail.com>
2024-12-15 19:31:15 +0000

    adding the time schema to the main blocks schema

--- server/routes/getBlocks.js
+++ server/routes/getBlocks.js
@@ -19,27 +19,3 @@ router.get("/:blockName", async (req, res) => {
 });
 
 export default router;
-
-// import express from "express";
-// import Block from "../modules/Block";
-// const router = express.Router();
-
-// // Route to get block data by blockName
-// router.get("/:blockName", async (req, res) => {
-//   const { blockName } = req.params;
-
-//   try {
-//     const block = await Block.findOne({ blockName }); // Find block by name
-
-//     if (!block) {
-//       return res.status(404).json({ error: "Block not found" });
-//     }
-
-//     res.status(200).json(block); // Return block data
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
-
-// export default router;
